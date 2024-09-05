74addc4a6635acb582fc397fe4990003322750fa
ntet2001 <manyamaigor2001@gmail.com>
2024-09-05 01:41:57 +0100

    version of the 05-09-2024 at 01:41, Hero component in progress

--- src/jumba-a-art-frontend/src/components/Hero.jsx
+++ src/jumba-a-art-frontend/src/components/Hero.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='bg-white rounded-lg'>
+        <div>
+            <h1>Discover African Digital Art & <br/> Collect Jumba'a Art</h1>
+            <p>With Jumba'a marketplace. More than 20k artists and Collectors, Create,sell and buy NFT art.</p>
+            <a className='bg-[#ff8c27] text-white text-center' href="/">Create Jumba'a Art</a>
+        </div>
+    </section>
   )
 }
 
